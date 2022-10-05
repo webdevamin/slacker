f3d989e9aaa74ea6c91a82307d821e23a5f9c179
1chbinamin <amin.m.intichev@gmail.com>
2022-10-05 23:19:55 +0200

    protect secret

--- pages/api/send.js
+++ pages/api/send.js
@@ -11,12 +11,9 @@ export default async function handler(req, res) {
     return res.status(404).json({ message: `You are not authorized` });
   }
 
-  await axios.post(
-    "https://hooks.slack.com/services/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",
-    {
-      text: "Join video call for John Doe: https://video-call-54422.com",
-    }
-  );
+  await axios.post(process.env.SLACK_WEBHOOK_URL, {
+    text: "Join video call for John Doe: https://video-call-54422.com",
+  });
 
   return res.status(200).json({
     message: `Done`,
